6989049b60b8d739d3ebb213dd4082ab071f36cb
charanvadhyar <142227706+charanvadhyar@users.noreply.github.com>
2026-06-21 14:57:23 +0530

    Create Implement Orthogonal Projection of a Vector onto a Line.py

---
+++ Implement Orthogonal Projection of a Vector onto a Line.py
@@ -0,0 +1,17 @@
+import numpy as np
+def orthogonal_projection(v, L):
+	"""
+	Compute the orthogonal projection of vector v onto line L.
+
+	:param v: The vector to be projected
+	:param L: The line vector defining the direction of projection
+	:return: List representing the projection of v onto L
+	"""
+
+	v = np.array(v)
+	L = np.array(L)
+
+	c = (v @ L) / (L @  L)
+	return c * L
+
+	pass
